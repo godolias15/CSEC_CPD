749d0c0c1c7081c4f62dec04d1e95038e2020a9d
godolias15 <159550632+godolias15@users.noreply.github.com>
2025-02-05 00:03:27 +0300

    Update Anton and Danik.cpp

--- Anton and Danik.cpp
+++ Anton and Danik.cpp
@@ -1,32 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n;
-    cin>>n;
+    cin >> n; 
     string s;
-    cin>>s;
-    int countA=0,countD= 0;
-    for (int  i =0; i<s.length(); i++){
-        if(s[i] == 'A'){
-            countA++;
+    cin >> s;  
+    
+    int countA = 0, countD = 0;
 
-        }else{
-            countD++;
-
-        }
-               
+    for (char ch : s) {  
+        if (ch == 'A') countA++;
+        else countD++;
     }
+
+   
+    if (countA > countD) {
+        cout << "Anton";
+    } else if (countD > countA) {
+        cout << "Danik";
+    } else {
+        cout << "Friendship";
+    }
+
+    return 0;  
 }
-if(countA>countD){
-    cout<<"Anton";
-
-}else if(countD>countA){
-    cout<<"Danik";
-
-}else{
-    cout<<"Friendship";
-
-return 0;
-}
-
