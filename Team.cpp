0cef9ba82db5a0949e905705126fb8d8838f24ff
godolias15 <159550632+godolias15@users.noreply.github.com>
2025-02-04 23:48:50 +0300

    Update Team.cpp

--- Team.cpp
+++ Team.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int g, c = 0;
+    cin >> g;
+
+    for (int i = 0; i < g; i++) {
+        int p, v, t;
+        cin >> p >> v >> t; 
+
+        if (p + v + t >= 2) { 
+            c++;
+        }
+    }
+
+    cout << c;
+    return 0;
+}
 
